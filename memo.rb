cf8d3f59f9c85cd2a5e4bc267e570e0239b83193
yuumizoi <y.mizoi@genbasupport.com>
2025-11-14 10:36:13 +0900

    不要なコメントとコメントアウトを削除

--- memo.rb
+++ memo.rb
@@ -1,12 +1,9 @@
 # frozen_string_literal: true
-
 require 'sinatra'
 require 'sinatra/contrib'
-# require 'sinatra/reloader'
 require 'json'
 require 'securerandom'
 
-# --- メソッドの定義 ---
 def load_memos
   File.open('memos.json') do |file|
     JSON.parse(file.read)
@@ -19,39 +16,32 @@ def save_memos(memos)
   end
 end
 
-# --- ルーティング (URI設計) ---
-# GET /memos (メモ一覧)
 get '/memos' do
   @memos = load_memos
   erb :index
 end
 
-# --- ヘルパーメソッドの定義 ---
 helpers do
   include Rack::Utils
   alias_method :h, :escape_html
 end
 
-# GET /memos/new（メモ作成画面）
 get '/memos/new' do
   erb :new
 end
 
-# GET /memos/:id（特定のメモを表示）
 get '/memos/:id' do
   memos = load_memos
   @memo = memos.find { |m| m['id'] == params['id'] }
   erb :show
 end
 
-# GET /memos/:id/edit (メモ編集画面)
 get '/memos/:id/edit' do
   memos = load_memos
   @memo = memos.find { |m| m['id'] == params['id'] }
   erb :edit
 end
 
-# PATCH /memos/:id (メモ編集・上書き保存)
 patch '/memos/:id' do
   memos = load_memos
   memo_to_update = memos.find { |m| m['id'] == params['id'] }
@@ -61,7 +51,6 @@ patch '/memos/:id' do
   redirect "/memos/#{params['id']}"
 end
 
-# DELETE /memos/:id（メモ削除）
 delete '/memos/:id' do
   memos = load_memos
   memos.delete_if { |m| m['id'] == params['id'] }
@@ -69,12 +58,10 @@ delete '/memos/:id' do
   redirect '/memos'
 end
 
-# 404 Not Found処理
 not_found do
   erb :not_found
 end
 
-# POST /memos（メモ作成）
 post '/memos' do
   title = params['title']
   content = params['content']
