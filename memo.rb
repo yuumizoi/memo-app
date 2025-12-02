fd233683ad758c46304e3649c68b5e16b36d26e3
yuumizoi <y.mizoi@genbasupport.com>
2025-12-02 17:35:16 +0900

    feat: Memoクラスにデータ参照機能(Read)を実装

--- memo.rb
+++ memo.rb
@@ -10,4 +10,23 @@ class Memo
     @title = title
     @content = content
   end
+
+  def self.all
+    sql = "SELECT id, title, content FROM memos ORDER BY created_at DESC;"
+    result = DatabaseConnection.query(sql)
+
+    result.map do |row|
+      Memo.new(id: row['id'], title: row['title'], content: row['content'])
+    end
+  end
+
+  def self.find(id)
+    sql = "SELECT id, title, content FROM memos WHERE id = $1;"
+    result = DatabaseConnection.query(sql, [id.to_i])
+
+    return nil if result.ntuples.zero?
+
+    row = result.first
+    Memo.new(id: row['id'], title: row['title'], content: row['content'])
+  end
 end
